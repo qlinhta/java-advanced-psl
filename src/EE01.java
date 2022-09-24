304c65e5073f64f74dfb107dcb89c2cfca4c0ddc
Quyen Linh TA <qlinhta@outlook.com>
2022-09-24 17:01:57 +0200

    Update :: Update exercice 1 @ Completed

--- src/EE01.java
+++ src/EE01.java
@@ -1,10 +1,29 @@
 public class EE01 {
 
     /*
-    2.	Ca affiche "System.out.println();"
-    3.	Il propose des méthodes
-    4.	Il propose des méthodes, des interfaces
-    5.	Il propose de créer des fonctions de base, des fonctions avancées où un constructeur
-    */
+    2.	What happens when you type sysout and press Ctrl + space in a main method ?
+    >> It recommends the System.out.println() method
+     */
+
+    /*
+    3. Same question with toStr then Ctrl + space inside a class ?
+    >> It recommends the toString() method
+     */
+
+    /*
+    4. Same question with main then Ctrl + space inside a class ?
+    >> It recommends the main() method
+     */
+
+    /*
+    5. Create a new int field called foo inside the class. What happens if you type Ctrl + space inside the class,
+    what if you now type set then press Ctrl + space ?
+    >> It offers to create basic functions, advanced functions or a constructor
+     */
+
+    /*
+    6. Select the class name. Que What happens if you type Alt+Shift+R ? Same question with the int field foo.
+    >> It offers to rename the class or the field
+     */
 
 }
