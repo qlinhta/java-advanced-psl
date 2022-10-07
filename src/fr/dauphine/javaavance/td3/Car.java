50fd3f37df4686665db12f7181a2cd2e78e3cf55
Quyen Linh TA <qlinhta@outlook.com>
2022-10-07 14:29:28 +0200

    Update :: GarageTest all successful @ Completed

--- src/fr/dauphine/javaavance/td3/Car.java
+++ src/fr/dauphine/javaavance/td3/Car.java
@@ -175,4 +175,5 @@ public class Car {
         this.plate = plate;
     }
 
+
 }
--- src/fr/dauphine/javaavance/td3/Garage.java
+++ src/fr/dauphine/javaavance/td3/Garage.java
@@ -1,28 +1,33 @@
 package fr.dauphine.javaavance.td3;
-
-/*
-6. Write a class Garage to store instances of Car (think about the appropriate data structure for this).
-Add a method in Garage to add a car to the garage. Make it impossible to add a null car in the garage using Objects.requireNonNull().
- */
-
 public class Garage {
+
+    /*
+    6. Write a class Garage to store instances of Car (think about the appropriate data structure for this).
+    Add a method in Garage to add a car to the garage. Make it impossible to add a null car in the garage using Objects.requireNonNull().
+     */
+
+    private static int id = 0;
+    private int idGarage;
     private Car[] cars;
     private int nbCars;
-    private long totalValue;
 
-    /* Constructor for the 6th question
     public Garage() {
-        cars = new Car[10];
-        nbCars = 0;
+        this.idGarage = id++;
+        this.cars = new Car[10];
+        this.nbCars = 0;
     }
-    */
 
+    // Can't add a null car in the garage
     public void add(Car car) {
         if (car == null) {
-            throw new NullPointerException("Car must not be null");
+            throw new NullPointerException("Can't add a null car in the garage");
         }
-        this.cars[nbCars] = car;
-        this.nbCars++;
+        if (nbCars == cars.length) {
+            Car[] newCars = new Car[cars.length * 2];
+            System.arraycopy(cars, 0, newCars, 0, cars.length);
+            cars = newCars;
+        }
+        cars[nbCars++] = car;
     }
 
     /*
@@ -30,19 +35,8 @@ public class Garage {
     For the exercise, we use the number of instances created from the Garage class. (think of static fields).
      */
 
-    private static int nbGarages = 0;
-    private int id;
-
-    // Constructor for the 7th question, update by adding the id
-    public Garage() {
-        this.cars = new Car[10];
-        this.nbCars = 0;
-        this.id = nbGarages;
-        nbGarages++;
-    }
-
     public int getId() {
-        return id;
+        return idGarage;
     }
 
     /*
@@ -52,35 +46,34 @@ public class Garage {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Garage ").append(this.id).append(" : ").append(this.nbCars).append(" cars");
-        for (int i = 0; i < this.nbCars; i++) {
-            sb.append("Garage ").append(this.id).append(" : ").append(this.cars[i]);
+        sb.append("Garage ").append(idGarage).append(" : ").append(nbCars).append(" cars");
+        for (int i = 0; i < nbCars; i++) {
+            sb.append("\n\t").append(cars[i]);
         }
         return sb.toString();
     }
 
     /*
-    9. Add a method to calculate the value of a garage (sum of the value of the cars it contains).
+    9. Add a method to calculate the total value of a garage (sum of the value of the cars it contains).
      */
 
-    public long calculateValue() {
-        long totalValue = 0;
-        for (int i = 0; i < this.nbCars; i++) {
-            totalValue += this.cars[i].getValue();
+    public long value() {
+        long value = 0;
+        for (int i = 0; i < nbCars; i++) {
+            value += cars[i].getValue();
         }
-        return totalValue;
+        return value;
     }
-
 
     /*
     10. Write a method firstCarByBrand which takes a brand as parameter and returns the first car of this brand.
     What should we do if there is no car of this brand?
      */
 
-    public Object firstCarByBrand(String brand) {
-        for (int i = 0; i < this.nbCars; i++) {
-            if (this.cars[i].getBrand().equals(brand)) {
-                return this.cars[i];
+    public Car firstCarByBrand(String brand) {
+        for (int i = 0; i < nbCars; i++) {
+            if (cars[i].getBrand().equals(brand)) {
+                return cars[i];
             }
         }
         return null;
@@ -88,6 +81,10 @@ public class Garage {
 
 
     public long getValue() {
-        return totalValue;
+        long value = 0;
+        for (int i = 0; i < nbCars; i++) {
+            value += cars[i].getValue();
+        }
+        return value;
     }
 }
